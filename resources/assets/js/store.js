bfc6c43940f5d309ec2ff242d92cba6e407f56c7
muyong <muyong@muyongs-MacBook-Pro.local>
2018-11-13 17:33:11 +0800

    将vuex模块添加到数据存储器

--- resources/assets/js/store.js
+++ resources/assets/js/store.js
@@ -1,12 +1,17 @@
 /**
  * Adds the promise polyfill for IE 11
  */
+
 require('es6-promise').polyfill();
 
 import Vue from 'vue'
 import Vuex from "vuex"
 //使用 Vuex 作为数据存储器
 Vue.use(Vuex)
+import {cafes} from "./modules/cafes";
+
 export default new Vuex.Store({
-    modules: {}
+    modules: {
+        cafes
+    }
 });
