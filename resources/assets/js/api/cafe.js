8343f65f95a0531d0d2d0a4de2aebfb8503eb072
muyong <muyong@muyongs-MacBook-Pro.local>
2018-11-13 17:10:39 +0800

    配置axios与laravel数据通信->api->cafe.js

---
+++ resources/assets/js/api/cafe.js
@@ -0,0 +1,21 @@
+import {ROAST_CONFIG} from "../config";
+
+export default {
+    getCafes: function () {
+        return axios.get(ROAST_CONFIG.API_URL + '/cafes');
+    },
+    getCafe: function (cafeID) {
+        return axios.get(ROAST_CONFIG.API_URL + '/cafes/' + cafeID);
+    },
+    postAddNewCafe: function (name, address, city, state, zip) {
+        return axios.post(ROAST_CONFIG.API_URL + '/cafes',
+            {
+                name: name,
+                address: address,
+                city: city,
+                state: state,
+                zip: zip
+            }
+        );
+    }
+}
---
+++ resources/assets/js/config.js
@@ -0,0 +1,13 @@
+var api_url = '';
+
+switch (process.env.NODE_ENV) {
+    case 'development':
+        api_url = 'https://roast.dev/api/v1';
+        break;
+    case 'production':
+        api_url = 'https://roast.dev/api/v1';
+        break;
+}
+export const ROAST_CONFIG = {
+    API_URL: api_url,
+}
