c2437024b20e293212fc82b896da3b7a31dc1567
muyong <muyong@muyongs-MacBook-Pro.local>
2018-11-13 17:00:30 +0800

    cafe ApiController Add

--- app/Http/Controllers/API/cafesController.php
+++ app/Http/Controllers/API/cafesController.php
@@ -21,8 +21,17 @@
 
         }
 
-        public function postNewCafe()
-        {
+        public function postNewCafe(){
+            $cafe = new Cafe();
 
+            $cafe->name     = Request::get('name');//咖啡店的名字
+            $cafe->address  = Request::get('address');//咖啡店的地址
+            $cafe->city     = Request::get('city');//咖啡店所在城市
+            $cafe->state    = Request::get('state');//咖啡店所在的身份
+            $cafe->zip      = Request::get('zip');//邮政编码
+
+            $cafe->save();
+
+            return response()->json($cafe, 201);
         }
     }
